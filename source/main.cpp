6a9a5a291f58f3e676a6bf5465b1a812dd9ecf30
meindert <meindertvanduin99@gmail.com>
2022-12-28 20:08:29 +0100

    Add bgfx, bimg and bx to the project

--- source/main.cpp
+++ source/main.cpp
@@ -1,6 +1,16 @@
-#include <cstdio>
+/*
+ * Copyright 2011-2020 Branimir Karadzic. All rights reserved.
+ * License: https://github.com/bkaradzic/bgfx#license-bsd-2-clause
+ */
+
+#include <bgfx/bgfx.h>
+#include <bgfx/platform.h>
 
 auto main() -> int {
-    printf("Hello, World!\n");
+    bgfx::PlatformData pd;
+
+    bgfx::init();
+
+    bgfx::shutdown();
     return 0;
 }
