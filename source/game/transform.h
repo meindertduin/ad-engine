12b82208894104cfd78ae9b570dda68225aca12e
meindert <meindertvanduin99@gmail.com>
2023-02-07 21:11:47 +0100

    Add a basic tarnsform component

---
+++ source/game/transform.h
@@ -0,0 +1,31 @@
+#include "component.h"
+
+namespace game {
+    class Transform : public Component<Transform> {
+    public:
+        Transform() noexcept = default;
+
+        [[nodiscard]] ALWAYS_INLINE constexpr float x() const {
+            return mX;
+        }
+
+        [[nodiscard]] ALWAYS_INLINE constexpr float y() const {
+            return mY;
+        }
+
+        void move(float x, float y) {
+            mX += x;
+            mY += y;
+        }
+
+        void setPosition(float x, float y) {
+            mX = x;
+            mY = y;
+        }
+        
+    private:
+        float mX = 0.0f;
+        float mY = 0.0f;
+    };
+
+}
