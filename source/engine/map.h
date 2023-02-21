cdf5d27332212cb7f5a97713e67e0829102ee373
meindert <meindertvanduin99@gmail.com>
2023-02-21 17:46:29 +0100

    Add a 32 bit hash

--- source/engine/map.h
+++ source/engine/map.h
@@ -1,5 +1,13 @@
 #pragma once
 
+#include <cstdint>
+
+template<typename T>
+struct HashFunc {
+    auto operator()(const T &value) const -> uint32_t {
+        return value;
+    }
+};
 
 template<typename K, typename T>
 class HashMap {
